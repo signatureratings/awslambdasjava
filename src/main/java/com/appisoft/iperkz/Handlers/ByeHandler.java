54be46838d33fa5e9390e31b5ae9908417dd369e
sairam balu <balusairam26@gmail.com>
2024-10-19 01:20:41 -0400

    first commit

---
+++ src/main/java/com/appisoft/iperkz/Handlers/ByeHandler.java
@@ -0,0 +1,22 @@
+package com.appisoft.iperkz.Handlers;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.http.HttpStatusCode;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+
+@Component
+public class ByeHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.setStatusCode(200);
+        response.setBody("Bye, World!");
+        return response;
+    }
+}
